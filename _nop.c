cd9287472826e2d13b3413fa311776cd3e9c9178
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-01-04 21:41:42 +0000

    fixed bug

--- _nop.c
+++ _nop.c
@@ -3,12 +3,10 @@
 /**
  * _nop - implement the nop opcode
  * @stack: pointer to a stack node pointer
- * @line_number: variable representing the current line number
  * Return: 0 if success
 */
 
-void _nop(stack_t **stack, unsigned int line_number)
+void _nop(stack_t **stack)
 {
     (void)stack;
-    (void)line_number;
 }
