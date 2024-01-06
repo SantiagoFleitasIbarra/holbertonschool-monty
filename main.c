e3a09310bf1f1b75cd7245a7e89aad98b2796635
Stein2023 <martinstein989@gmail.com>
2024-01-06 11:09:42 -0800

    monty

--- main.c
+++ main.c
@@ -37,14 +37,23 @@ _push(&stack, line_number);
 	else if (strcmp(opcode, "pint") == 0)
 	_pint(&stack, line_number);
 	else if (strcmp(opcode, "pop") == 0)
+	{
 	pop(&stack, line_number);
+	}
 	else if (strcmp(opcode, "swap") == 0)
+	{
 	swap(&stack, line_number);
+	}
 	else if (strcmp(opcode, "add") == 0)
+	{
 	add(&stack, line_number);
+	}
 	else
 	{
 	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-		exit(EXIT_FAILURE); } }
+		exit(EXIT_FAILURE);
+	}
+	}
 	fclose(file), free_stack(stack), free_line(line);
-	return (0); }
+	return (0);
+}
--- monty.h
+++ monty.h
@@ -1,9 +1,9 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
